00e13f96273f24c29beee0b618dfb8224657e0db
Darshan Vora <darshanvora562@gmail.com>
2023-12-26 01:01:34 +0530

    Make file upload services

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -65,6 +65,67 @@ export class Service {
             return false;
         }
     }
+
+    async getPost(slug) {
+        try {
+            return await this.getPost(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+            )
+        } catch (error) {
+            console.log('Appwrite Service: getPost :: error', error);
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", 'active')]) {
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log('Appwrite Service: getPosts :: error', error);
+            return false;
+        }
+    }
+
+    // file upload services
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file,
+
+            )
+        } catch (error) {
+            console.log('Appwrite Service: uploadFile :: error', error);
+            return false;
+        }
+    }
+
+    async deleteFIle(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId,
+            );
+            return true;
+        } catch (error) {
+            console.log('Appwrite Service: deleteFIle :: error', error);
+            return false;
+        }
+    }
+
+    getFilePreview(fileId) {
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId,
+        )
+    } 
 }
 
 const service = new Service();
